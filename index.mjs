49cd9f77fc80b09f3281de10bb81688e328366cd
Khushi Mishra <misskhushi597@gmail.com>
2026-03-23 16:54:15 +0530

    root route

--- index.mjs
+++ index.mjs
@@ -1,6 +1,8 @@
 import express from "express";
 const server = express();
-
+server.get("/", (req, res) => {
+  res.json({ name: "apple" });
+});
 server.listen(3000, () => {
   console.log(`server started on ${port}`);
 });
