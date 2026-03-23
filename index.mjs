55ebff74fa4b486eb307e0535ee3db56fac2c068
Khushi Mishra <misskhushi597@gmail.com>
2026-03-23 16:58:07 +0530

    hello route

--- index.mjs
+++ index.mjs
@@ -3,6 +3,9 @@ const server = express();
 server.get("/", (req, res) => {
   res.json({ name: "apple" });
 });
+server.get("hello", (req, res) => {
+  res.json({ name: "banana" });
+});
 server.listen(3000, () => {
   console.log(`server started on ${port}`);
 });
